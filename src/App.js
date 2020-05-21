48695b357bd6b1f777cd51f320492c2c31de5687
singharpan09 <arpan@excellencetechnologies.info>
2020-05-21 17:58:08 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,68 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "bootstrap/dist/css/bootstrap.css";
 
-import Button from "react-bootstrap/Button";
+import { Button, FormControl, InputGroup, Badge } from "react-bootstrap";
 import "./App.css";
 
 function App() {
+  const [todo, settodo] = useState("");
+  const [todoList, settodoList] = useState([]);
+
+  const addtodo = (todo) => {
+    const newTodo = [
+      ...todoList,
+      { id: Date.now(), todo: todo, completed: false },
+    ];
+    settodo("");
+    settodoList(newTodo);
+  };
+  const handlechange = (todo) => {
+    const newTodo = [...todoList];
+    const Index = newTodo.indexOf(todo);
+    console.log(Index);
+    newTodo[Index].completed =
+      newTodo[Index].completed === false ? true : false;
+    settodoList(newTodo);
+  };
+
   return (
     <div className="App">
       <h3>#Todo's for the day</h3>
-      <form>
-        <input type="text" />
-        <br />
-        <Button variant="success">Submit</Button>
-      </form>
+
+      <InputGroup className="mb">
+        <FormControl
+          required={true}
+          placeholder="Enter Todos...."
+          value={todo}
+          onChange={(e) => settodo(e.target.value)}
+        />
+      </InputGroup>
+
+      <Button
+        disabled={todo.length === 0 ? true : false}
+        onClick={() => {
+          addtodo(todo);
+        }}
+        variant="success"
+      >
+        Submit
+      </Button>
+
+      {todoList.map((todo) => (
+        <p key={todo.id}>
+          {todo.todo}
+          <input
+            className="checked"
+            type="checkbox"
+            checked={todo.completed}
+            onChange={() => handlechange(todo)}
+          />
+          {todo.completed === true && (
+            <Badge variant="secondary">completed</Badge>
+          )}
+        </p>
+      ))}
     </div>
   );
 }
