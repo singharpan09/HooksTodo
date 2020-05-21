773fd363ab746f30ee8fe9e5326fe249524e72ed
singharpan09 <arpan@excellencetechnologies.info>
2020-05-21 15:54:05 +0530

    few fixes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React from "react";
 
+import "bootstrap/dist/css/bootstrap.css";
+
+import Button from "react-bootstrap/Button";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <h3>#Todo's for the day</h3>
-      <form onSubmit>
+      <form>
         <input type="text" />
         <br />
-        <button type="submit">Submit</button>
+        <Button variant="success">Submit</Button>
       </form>
     </div>
   );
 }
 
 export default App;
+//need to install both "bootstrap" & "react-bootstrap"
